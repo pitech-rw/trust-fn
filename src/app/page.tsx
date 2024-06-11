d89e1b4a582f37d70ec220fe6c31857cb0eb3fcd
ceelogre <sam.nishimwe@yahoo.com>
2024-06-11 18:34:15 +0200

    feat: boilerplate

---
+++ src/app/page.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+      </div>
+
+      <div className={styles.center}>
+        <h1>Trust</h1>
+      </div>
+
+      <div className={styles.grid}>
+        <a
+          href="#"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Get certified taskers <span>-&gt;</span>
+          </h2>
+          <p>
+            Request for a service provider and get unmatched experience
+          </p>
+        </a>
+      </div>
+    </main>
+  );
+}
